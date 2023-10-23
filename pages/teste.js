e3a85dbf27f155e7cae183802b5d0fb91ce9cccb
ArthurEv <arthurev.sg034@academico.ifsul.edu.br>
2023-10-23 08:23:51 -0300

    teste

--- pages/teste.js
+++ pages/teste.js
@@ -2,6 +2,10 @@ import MenuTeste from "../components/MenuTeste.js"
 
 export default function Teste(){
     return(
+        <>        
         <MenuTeste/>
+        <div>teste</div>
+        </>
+
     )
 }
